222d36ba59f394863a95ea6546c25993d6526d36
HongKi Cho <whghdrl9977@gmail.com>
2023-12-30 02:44:07 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 644 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    
+    map<int, int> map1;
+    int N, M, tmp;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> tmp;
+        if (map1.find(tmp) != map1.end()) {
+            map1[tmp]++;
+        }
+        else {
+            map1.insert(make_pair(tmp, 1));
+        }
+    }
+    
+    cin >> M;
+
+    for (int i = 0; i < M; i++) {
+        cin >> tmp;
+        if (map1.find(tmp) != map1.end()) {
+            cout << map1[tmp] << ' ';
+        }
+        else {
+            cout << 0 << ' ';
+        }
+    }
+}
