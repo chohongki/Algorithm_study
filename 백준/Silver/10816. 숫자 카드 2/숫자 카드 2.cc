55902ae4e44dd12c5d8fac4979e4d69ce52fe9ea
HongKi Cho <whghdrl9977@gmail.com>
2023-12-30 02:48:44 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 308 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,35 +1,32 @@
 #include <iostream>
-#include <unordered_map>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int N, M, a, b;
 
-    unordered_map<int, int> map1;
-    int N, M, tmp;
+	vector<int> v;
 
-    cin >> N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
 
-    for (int i = 0; i < N; i++) {
-        cin >> tmp;
-        if (map1.find(tmp) != map1.end()) {
-            map1[tmp]++;
-        }
-        else {
-            map1.insert(make_pair(tmp, 1));
-        }
-    }
-    
+	sort(v.begin(), v.end());
+	
     cin >> M;
-
-    for (int i = 0; i < M; i++) {
-        cin >> tmp;
-        if (map1.find(tmp) != map1.end()) {
-            cout << map1[tmp] << ' ';
-        }
-        else {
-            cout << 0 << ' ';
-        }
-    }
+	for (int i = 0; i < M; i++) {
+		cin >> b;
+		auto lower = lower_bound(v.begin(), v.end(), b);
+		auto upper = upper_bound(v.begin(), v.end(), b);
+		cout << upper - lower << " ";
+	}
+	return 0;
 }
