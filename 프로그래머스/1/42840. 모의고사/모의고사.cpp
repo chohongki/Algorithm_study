0db04eeeb1f365e3399164c2082cb24c618119c4
HongKi Cho <whghdrl9977@gmail.com>
2026-04-06 14:33:53 +0900

    [level 1] Title: 모의고사, Time: 0.35 ms, Memory: 4.56 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    vector<int> correct = {0, 0, 0};
+    
+    const vector<vector<int>> supos = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+    
+    for (int i = 0; i < answers.size(); i++) {
+        for (int j = 0; j < 3; j++) {
+            if (answers[i] == supos[j][i % supos[j].size()]) correct[j]++;
+        }
+    }
+    
+    int mxm = *max_element(correct.begin(), correct.end());
+    for (int i = 0; i < 3; i++) {
+        if (correct[i] == mxm) answer.emplace_back(i+1);
+    }
+    
+    return answer;
+}
