612e5a70056c8e8027d5ce6675f237fc7ff67d2a
HongKi Cho <whghdrl9977@gmail.com>
2026-04-06 14:35:17 +0900

    [level 1] Title: 모의고사, Time: 0.34 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    vector<int> correct = {0, 0, 0};
+    vector<int> correct(3);
     
     const vector<vector<int>> supos = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
     
